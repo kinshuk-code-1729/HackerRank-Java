6b20cf0a613b5fe45c574ce54fa4ad787373fdbd
Kinshuk Banerjee <kinshuk2002.king@gmail.com>
2022-10-26 19:17:55 +0530

    Add files via upload

---
+++ hrkjava6.java
@@ -0,0 +1,12 @@
+// Java End-of-file
+import java.util.Scanner;
+public class hrkjava6 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int k = 0;
+        while(s.hasNext()){
+            k++;
+            System.out.println(k+" "+s.nextLine());
+        }
+    }
+}
